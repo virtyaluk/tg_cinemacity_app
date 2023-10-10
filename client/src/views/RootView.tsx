39236993057c3e97fe9d84e6605bbf33c038d2eb
virtyaluk <bohdan@modern-dev.com>
2023-10-10 22:30:08 +0300

    fix: fixes app starting route.

--- client/src/views/RootView.tsx
+++ client/src/views/RootView.tsx
@@ -47,19 +47,19 @@ export default function RootView(): JSX.Element {
 
         // navigate('/movies/792293');
         // navigate(APP_ROUTES.MOVIES_LIST_ROUTE);
-        navigate(APP_ROUTES.TOUR_ROUTE);
+        // navigate(APP_ROUTES.TOUR_ROUTE);
         // navigate(APP_ROUTES.TICKETS_ROUTE);
         // navigate('movies/897087/booking/');
 
-        // appController.storage
-        //     .getItem(APP_TOUR_FINISHED_KEY)
-        //     .then(result => {
-        //         if (result && result == 'true') {
-        //             navigate('/movies');
-        //         } else {
-        //             navigate('/tour');
-        //         }
-        //     });
+        app.storage
+            .getItem(APP_TOUR_FINISHED_KEY)
+            .then(result => {
+                if (result && result == 'true') {
+                    navigate(APP_ROUTES.MOVIES_LIST_ROUTE);
+                } else {
+                    navigate(APP_ROUTES.TICKETS_ROUTE);
+                }
+            });
 
         app
             .ready()
